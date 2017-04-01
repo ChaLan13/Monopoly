0db2de9cc137b86faa8dd0d4fcadaaedf6076095
lukyky414 <lucassschwab.e@gmail.com>
2017-04-01 16:36:58 +0200

    Simplifier paquet

--- src/common/Paquet.java
+++ src/common/Paquet.java
@@ -7,30 +7,22 @@ import common.Carte;
 
 public class Paquet{
 	private ArrayList<Carte> pile;
-	private int max = 1;
-	private int nbr = 0;
 	
-	public Paquet(int max){
-		if(max < 1){
-			System.exit(2);
-		}
-		this.max = max;
+	public Paquet(){
 		pile = new ArrayList<Carte>();
 	}
 	
-	public boolean add(Carte carte) throws IndexOutOfBoundsException{
-		if(nbr < max && carte != null){
-			nbr++;
-			return pile.add(carte);
+	public void add(Carte carte){//TODO throws
+		if(carte != null){
+			pile.add(carte);
 		}
 		else
-			throw new IndexOutOfBoundsException("Too much object");
+			//TODO exception carte null
 	}
 	
 	public Carte tirer() throws IndexOutOfBoundsException{
 		Carte res = pile.get(0);
 		pile.remove(0);
-		nbr--;
 		return res;
 	}
 	
